174ac94e181a3e4f5e0c9e2e34716ba5dcabcb61
bbt <2051053963@qq.com>
2022-04-17 16:27:26 +0800

    first commit

---
+++ src/main/java/com/bbt/gitTest.java
@@ -0,0 +1,11 @@
+package com.bbt;
+
+/**
+ * @author bbt
+ * @create 2022-04-17-16   16:25
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
